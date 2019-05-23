ea71b8b6974d82de322c45c3a9a3a047ca14f33d
Elise Symmes <elisesymmes@gmail.com>
2019-05-23 17:53:11 -0400

    Code cleanup

--- functions.py
+++ functions.py
@@ -2,19 +2,20 @@ import random
 import numpy as np
 import sys
 import math
+import matplotlib.pyplot as plot
 
 
-def genPoint(d, coordLowerBound, coordUpperBound):
+def genPoint(d, coordSize):
     x = np.zeros(shape=d)
     for i in range(0, d):
-        x[i] = random.uniform(coordLowerBound, coordUpperBound)
+        x[i] = random.uniform(-1 * coordSize / 2, coordSize / 2)
     return x
 
 
-def genSetPoints(d, num, coordLowerBound, coordUpperBound):
+def genSetPoints(d, num, coordSize):
     ret = np.zeros(shape=(num, d))
     for i in range(0, num):
-        ret[i] = genPoint(d, coordLowerBound, coordUpperBound)
+        ret[i] = genPoint(d, coordSize)
     return ret
 
 
@@ -23,15 +24,15 @@ def p(pnt, s, a):
     for i in pnt:
         total += i
     random.seed(total + s + a)
-    normal = random.uniform(0, math.fabs(s*a/4))
-    weights = np.random.rand(len(pnt))
+    normal = random.uniform(0, math.fabs(s * a / 4))
     normal *= normal
+    weights = np.random.rand(len(pnt))
     weightsTotal = 0.0
     for i in weights:
         weightsTotal += i
     ret = np.zeros(shape=pnt.shape)
     for i in range(0, len(ret)):
-        ret[i] = math.sqrt(normal*(weights[i]/weightsTotal))
+        ret[i] = math.sqrt(normal * (weights[i] / weightsTotal))
         if random.randrange(0, 2) == 1:
             ret[i] *= -1
     return ret
@@ -39,9 +40,9 @@ def p(pnt, s, a):
 
 def encryptSingle(x, s, a):
     ret = np.zeros(shape=x.shape)
-    pVals = p(x, s, a)
+    pVal = p(x, s, a)
     for i in range(0, len(x)):
-        ret[i] = x[i] * s + pVals[i]
+        ret[i] = x[i] * s + pVal[i]
     return ret
 
 
@@ -72,3 +73,40 @@ def nearestNeighbor(point, neighbors):
             nearestIndex = n
             nearestDist = dist
     return nearestIndex
+
+
+def plotPDist(num):
+    portions = np.zeros(shape=num)
+    for i in range(0, num):
+        s = random.uniform(-2.0, 2.0)
+        a = random.uniform(-1.0, 1.0)
+        x = genPoint(3, 10)
+        pVal = p(x, s, a)
+        portions[i] = math.fabs((np.linalg.norm(pVal) - math.fabs(s * a / 4)) / (s * a / 4))
+    plot.hist(portions)
+    plot.suptitle("Distribution of p values divided by sa/4")
+    plot.show()
+
+
+def plotErrorDistance(s, a, coordSize, dim, numNeighbors, trials):
+    neighborhood = genSetPoints(dim, numNeighbors, coordSize)
+    encNeighborhood = encryptArr(neighborhood, s, a)
+    offBy = np.zeros(shape=trials)
+    numDif = 0
+    for i in range(0, trials):
+        point = genPoint(dim, coordSize)
+        encPoint = encryptSingle(point, s, a)
+        normNeighbor = nearestNeighbor(point, neighborhood)
+        encNeighbor = nearestNeighbor(encPoint, encNeighborhood)
+        if normNeighbor != encNeighbor:
+            numDif += 1
+            offBy[i] = math.fabs(distance(point, neighborhood[normNeighbor]
+                                          - distance(point, neighborhood[encNeighbor])))
+    if numDif > 0:
+        offBy = offBy.take(offBy.nonzero()).reshape(numDif)
+        plot.hist(offBy, color='blue')
+        plot.suptitle("Distance from correct neighbor to selected neighbor\n" +
+                      str(numDif) + " errors from " + str(trials) + " trials within "
+                      + str(coordSize) + " side length cube")
+        plot.show()
+    return numDif
--- main.py
+++ main.py
@@ -1,41 +1,5 @@
 import random
-import numpy as np
-import matplotlib.pyplot as plot
 import functions as fn
-import math
 
-s = random.uniform(-2.0, 2.0)
-a = random.uniform(-1.0, 1.0)
-coordLowerBound = -100.0
-coordUpperBound = 100.0
-dim = 3
-numNeighbors = 100
-trials = 100
-
-neighborhood = fn.genSetPoints(dim, numNeighbors, coordLowerBound, coordUpperBound)
-encNeighborhood = fn.encryptArr(neighborhood, s, a)
-offBy = np.zeros(shape=trials)
-offByEnc = np.zeros(shape=trials)
-numSame = 0
-numDif = 0
-for i in range(0, trials):
-    point = fn.genPoint(dim, coordLowerBound, coordUpperBound)
-    encPoint = fn.encryptSingle(point, s, a)
-    normNeighbor = fn.nearestNeighbor(point, neighborhood)
-    encNeighbor = fn.nearestNeighbor(encPoint, encNeighborhood)
-    if normNeighbor == encNeighbor:
-        numSame += 1
-    else:
-        numDif += 1
-        offBy[i] = math.fabs(fn.distance(point, neighborhood[normNeighbor]
-                                         - fn.distance(point, neighborhood[encNeighbor])))
-        offByEnc[i] = -1 * math.fabs(fn.distance(point, encNeighborhood[normNeighbor]
-                                                 - fn.distance(point, encNeighborhood[encNeighbor])))
-print("Error rate of", str(float(numDif)*100.0/trials) + "%")
-if numDif > 0:
-    offBy = offBy.take(offBy.nonzero()).reshape(numDif)
-    plot.hist(offBy, color='blue')
-    plot.suptitle("Distance from correct neighbor to selected neighbor\n" +
-                  str(numDif) + " errors from " + str(trials) + " trials within "
-                  + str(coordUpperBound - coordLowerBound) + " side length cube")
-    plot.show()
+fn.plotErrorDistance(random.uniform(-2.0, 2.0), random.uniform(-1.0, 1.0), 100, 3, 1000, 1000)
+# fn.plotPDist(5000)
