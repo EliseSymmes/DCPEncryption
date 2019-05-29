6b2a6e369444dbf8e0c97ced5d686ed4559b30d5
Elise Symmes <elisesymmes@gmail.com>
2019-05-29 17:13:15 -0400

    Cleaned up main

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 
-cal = np.loadtxt("C:\\Users\\Elise\\PycharmProjects\\DCPEncryption\\venv\\cal.txt")
-cal = np.delete(cal, 0, 1)
-rateSigma, distSigma = fn.statsSigmaRange(cal, 150, 2000, 1, 50, 50)
+cal = np.delete(np.loadtxt("C:\\Users\\Elise\\PycharmProjects\\DCPEncryption\\venv\\cal.txt"), 0, 1)
+querySize = 150
+neighborSize = 2000
+sigmaLower = 1
+sigmaUpper = 50
+trials = 50
+rateSigma, distSigma = fn.statsSigmaRange(cal, querySize, neighborSize, sigmaLower, sigmaUpper, trials)
 rateIndex = np.argmin(rateSigma)
 errorIndex = np.argmin(distSigma)
 print("Min error rate at", rateIndex, "with value", rateSigma[rateIndex],
       "\nMin mean error distance at", errorIndex, "with value", distSigma[errorIndex])
-fifty = np.zeros(shape=50)
+axis = np.zeros(shape=50)
 for i in range(0, 50):
-    fifty[i] = i + 1
-plot.scatter(fifty, rateSigma)
+    axis[i] = i + 1
+plot.scatter(axis, rateSigma)
 plot.title("Error rate as a function of sigma")
 plot.show()
 plot.clf()
-plot.scatter(fifty, distSigma)
+plot.scatter(axis, distSigma)
 plot.title("Mean distance of error as a function of sigma")
 plot.show()
